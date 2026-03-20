254831cd42399e2f3480e89694887c6f97a4f60c
PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>
2026-03-20 10:05:02 -0500

    refactor. updated provision.sh args
    Signed-off-by: PostboxRetinal <78338192+PostboxRetinal@users.noreply.github.com>

--- provision.sh
+++ provision.sh
@@ -2,10 +2,12 @@
 # provision.sh - minikube provisioning script
 
 set -e
+export DEBIAN_FRONTEND=noninteractive
+
 echo "Starting minikube + kubectl provisioning script..."
 
-apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+sudo apt update -y
+sudo apt install -y apt-transport-https ca-certificates curl gnupg
 echo "Installing minikube"
 
 curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
@@ -27,6 +29,10 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 
 echo "4) Installing kubectl..."
-sudo apt-get update
-sudo apt-get install -y kubectl
+sudo apt update
+sudo apt install -y kubectl
+
+echo "5) Cleaning up..."
+sudo apt autoremove -y
+
 echo "kubectl installed successfully :)"
